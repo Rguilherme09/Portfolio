7514426cf31d8b5bb8e9f24da2a019c943b3d1a3
Rodrigo Guilherme <rguilherme419@hotmail.com>
2025-07-02 18:05:13 -0300

    atualização de banner responsivo

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
+import { useState, useEffect } from "react";
 import styles from "./page.module.css";
 import CarrosselProjetos from "@/componentes/Carrossel/CarrosselProjetos.jsx";
 import Sobre from "@/componentes/Sobre/sobre.jsx";
-import Habilidades from '@/componentes/Habilidades/habilidades.jsx';
-import Contato from '@/componentes/Contato/contato.jsx';
-
+import Habilidades from "@/componentes/Habilidades/habilidades.jsx";
+import Contato from "@/componentes/Contato/contato.jsx";
 
 export const metadata = {
   title: "RodrigoDev",
@@ -11,16 +11,42 @@ export const metadata = {
 };
 
 export default function Home() {
-  return (
+  const [mostrarSecoes, setMostrarSecoes] = useState(true);
 
-    
-      <main className={styles.main}>
-          <section className={styles.primeira_secao}>
-            <h1>Rodrigo<br/>Guilherme</h1>
-            <p className={styles.slogan}>Desenvolvedor Front-End | Next.js | React.js | Node.js</p>
-          </section>
-          <section id="sobre" className={styles.segunda_secao}>  
-            <Sobre />     
+  useEffect(() => {
+    if (window.innerWidth <= 768) {
+      setMostrarSecoes(false);
+
+      const timeout = setTimeout(() => setMostrarSecoes(true), 6000);
+
+      const onScroll = () => setMostrarSecoes(true);
+
+      window.addEventListener("scroll", onScroll);
+
+      return () => {
+        clearTimeout(timeout);
+        window.removeEventListener("scroll", onScroll);
+      };
+    }
+  }, []);
+
+  return (
+    <main className={styles.main}>
+      <section className={styles.primeira_secao}>
+        <h1>
+          Rodrigo
+          <br />
+          Guilherme
+        </h1>
+        <p className={styles.slogan}>
+          Desenvolvedor Front-End | Next.js | React.js | Node.js
+        </p>
+      </section>
+
+      {mostrarSecoes && (
+        <>
+          <section id="sobre" className={styles.segunda_secao}>
+            <Sobre />
           </section>
           <section id="projetos" className={styles.terceira_secao}>
             <h1>Projetos</h1>
@@ -29,9 +55,11 @@ export default function Home() {
           <section id="habilidades" className={styles.quarta_secao}>
             <Habilidades />
           </section>
-          <section id ="contato" className={styles.quinta_secao}>
+          <section id="contato" className={styles.quinta_secao}>
             <Contato />
           </section>
-      </main>
+        </>
+      )}
+    </main>
   );
 }
